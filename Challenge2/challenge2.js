255c4a4f7916f87f91c991da211de8612afc7474
Angel <stixaw@gmail.com>
2019-03-21 21:51:26 -0600

    First Commit

---
+++ Challenge2/challenge2.js
@@ -0,0 +1,65 @@
+require('chromedriver');
+var webdriver = require('selenium-webdriver');
+var assert = require("chai").assert;
+
+describe("challenge1 suite", function(){
+    this.timeout(20000);
+    var driver;
+
+    before(function () {
+    // initializing chrome driver
+       driver = new webdriver.Builder()
+       .withCapabilities(webdriver.Capabilities.chrome())
+       .build();
+    });
+
+    after(function () {
+        return driver.quit();
+    });
+
+    it("It should open the copart website", function() {
+        return driver.get("http://www.copart.com");
+    });
+    
+    // it("The title is 'Auto Auction - Copart USA - Salvage Cars For Sale'", function() {
+    //     // Since we want the title from the page, we need to manually handle the Promise
+    //     return driver.getTitle().then(function(title) {
+    //         assert.include("Auto Auction - Copart USA");
+    //     });
+    // });
+    // If you don’t want to use promises, you can use awaits.  
+    // This is easier to understand than to use promise…. then(function()).
+    it("The title is 'Auto Auction - Copart USA - Salvage Cars For Sale'", async function() {
+        var title = await driver.getTitle();
+        return assert.include("Auto Auction - Copart USA");
+    });
+
+    // have to do find element
+    it("Should search on copart for porschel", async function(){
+        var element = await driver.findElement(By.Id("input-search"))
+        return element.sendKeys("Exotic" + Key.ENTER)
+    });
+
+    // it("Should run search on google for porsche", async function() {
+    //     var element = await driver.findElement(By.name("q"));
+    //     return element.sendKeys("Porsche" + Key.ENTER)
+    // });
+
+    it("Should assert 911 is in list of results", async function() {
+        await driver.wait(until.titleContains('Porsche'), 10000);
+        console.log(await driver.getTitle());
+        var html = await driver.findElement(By.tagName("body")).getAttribute('innerHTML');
+        // console.log(html);
+        return assert.include(html, "911");
+    });
+
+
+});
+
+
+
+
+
+
+
+
