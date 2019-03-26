22b5730997a5b009d65ac28e3cfaf004619c7369
stixaw <stixaw@gmil.com>
2019-03-26 16:04:01 -0600

    Start of Challenge 3

--- Challenge3/challenge3.js
+++ Challenge3/challenge3.js
@@ -1,10 +1,11 @@
 require('chromedriver');
-var webdriver = require('selenium-webdriver');
-var wdriver = webdriver.*;
+const {Builder, By, until, Key} = require('selenium-webdriver');
 var assert = require("chai").assert;
-var By = webdriver.By;
-var until = webdriver.until;
-var Key = webdriver.Key;
+// var By = webdriver.By;
+// var until = webdriver.until;
+// var Key = webdriver.Key;
+
+
 
 describe("challenge1 suite", function(){
     this.timeout(20000);
@@ -12,50 +13,48 @@ describe("challenge1 suite", function(){
 
     before(function () {
     // initializing chrome driver
-       driver = new webdriver.Builder()
-       .withCapabilities(webdriver.Capabilities.chrome())
-       .build();
-       var By = driver.By;
-
+    //    driver = new webdriver.Builder()
+    //    .withCapabilities(webdriver.Capabilities.chrome())
+    //    .build();
+        driver = new Builder().forBrowser('chrome').build();
     });
 
     after(function () {
         return driver.quit();
     });
 
-    it("It should open the google website", function() {
-        return driver.get("http://www.google.com");
+    it("It should open the copart website", function() {
+        return driver.get("http://www.copart.com");
     });
     
-    // it("The title is 'Google'", function() {
+    // it("The title is 'Auto Auction - Copart USA - Salvage Cars For Sale'", function() {
     //     // Since we want the title from the page, we need to manually handle the Promise
     //     return driver.getTitle().then(function(title) {
-    //         assert.equal(title, "Google");
+    //         assert.include("Auto Auction - Copart USA");
     //     });
     // });
     
     // If you don’t want to use promises, you can use awaits.  
     // This is easier to understand than to use promise…. then(function()).
-    it("The title is 'Google'", async function() {
+    it("The title is 'Auto Auction - Copart USA - Salvage Cars For Sale'", async function(){
         var title = await driver.getTitle();
-        return assert.equal(title, "Google");
+        return assert.include(title, "Auto Auction - Copart USA");
     });
 
-    it("Should run search on google for porsche", async function() {
-        var element = await driver.findElement(By.name("q"));
-        return element.sendKeys("Porsche" + Key.ENTER)
+    // have to do find element
+    it("Should find the Most Popular Items", async function(){
+        var element = await driver.findElement(By.className("col-lg-3 col-sm-3 col-md-3 col-xs-6 col-xs-ext-sm"));
+        return assert.equal
+        //return element.sendKeys("Exotic" + Key.ENTER);
     });
 
-    it("Should assert 911 is in list of results", async function() {
-        await driver.wait(until.titleContains('Porsche'), 10000);
+    it("Should assert Porsche is in list of results", async function() {
+        await driver.wait(until.titleContains('Auto Auction - Copart USA'), 12000);
         console.log(await driver.getTitle());
         var html = await driver.findElement(By.tagName("body")).getAttribute('innerHTML');
         // console.log(html);
-        return assert.include(html, "911");
+        return assert.include(html, "Porsche");
     });
+
+
 });
-
-
-
-
-
