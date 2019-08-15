51ac79bd3c5d0731f114acd83b2b1347c712da81
stixaw <stixaw@gmail.com>
2019-08-14 23:18:33 -0600

    updated challenge 4 and still working on 8

--- test-suites/challenge4.js
+++ test-suites/challenge4.js
@@ -4,14 +4,15 @@ const numToString = require('../helpers/convertNumToString')
 describe("challenge 4 Fibonacci Functions", () => {
     it('should make individual calls to methods', () => {
         console.log(fib.fibonacci(0) + "...." + numToString.numToStr(fib.fibonacci(0)))
+        console.log(fib.fibonacci(1) + "...." + numToString.numToStr(fib.fibonacci(1)))
+        console.log(fib.fibonacci(2) + "...." + numToString.numToStr(fib.fibonacci(2)))
+        console.log(fib.fibonacci(3) + "...." + numToString.numToStr(fib.fibonacci(3)))
+        console.log(fib.fibonacci(4) + "...." + numToString.numToStr(fib.fibonacci(4)))
         console.log(fib.fibonacci(5) + "...." + numToString.numToStr(fib.fibonacci(5)))
-        console.log(fib.fibonacci(15) + "...." + numToString.numToStr(fib.fibonacci(15)))
-        console.log(fib.fibonacci(20) + "...." + numToString.numToStr(fib.fibonacci(20)))
-        console.log(fib.fibonacci(25) + "...." + numToString.numToStr(fib.fibonacci(25)))
-        console.log(fib.fibonacci(35) + "...." + numToString.numToStr(fib.fibonacci(35)))
+        console.log(fib.fibonacci(6) + "...." + numToString.numToStr(fib.fibonacci(6)))
     })
 
-    it('should call a single function with a parameter of 30', () => {
-        fib.fib_series(30)
-    })
+    // it('should call a single function with a parameter of 30', () => {
+    //     fib.fib_series(30)
+    // })
 })
