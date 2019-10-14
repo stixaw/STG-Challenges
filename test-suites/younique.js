c2e044ecf6da1b4ed238cdf2d827ed61109eb693
Angel Williams <angel.williams@chghealthcare.com>
2019-10-14 11:21:05 -0600

    working Younique single row total with discount in play

--- test-suites/younique.js
+++ test-suites/younique.js
@@ -1,14 +1,14 @@
 require('chromedriver')
-var webdriver = require('selenium-webdriver')
-var assert = require('chai').assert
+const webdriver = require('selenium-webdriver')
+const assert = require('chai').assert
 const driverManager = require('../common/driver')
-var By = webdriver.By
-var until = webdriver.until
-var Key = webdriver.Key
+const By = webdriver.By
+const until = webdriver.until
+const Key = webdriver.Key
 
 describe('challenge 1 suite', function () {
   this.timeout(34000)
-  var driver
+  let driver
 
   before(() => {
     driver = driverManager.getDriver('chrome')
@@ -25,59 +25,122 @@ describe('challenge 1 suite', function () {
     })
 
     it('should assert we are on younqiue', async () => {
-      var title = await driver.getTitle()
+      let title = await driver.getTitle()
       await assert.include(title, 'Younique')
     })
 
     it('should add the item to the cart by clicking on the add to cart button', async () => {
-      var addToCartBtn = await driver.findElement(By.css(".addToCartBtn"))
+      let addToCartBtn = await driver.findElement(By.css(".addToCartBtn"))
       addToCartBtn.click()
 
       await driver.wait(until.elementLocated(By.css("#viewCart", 10000)))
-      var viewCartBtn = await driver.findElement(By.css("#viewCart"))
+      let viewCartBtn = await driver.findElement(By.css("#viewCart"))
       viewCartBtn.click()
 
       await driver.wait(until.elementsLocated(By.xpath("//h1[.='My Shopping Cart']", 10000)))
-      var myCart = driver.findElement(By.xpath("//h1[.='My Shopping Cart']"))
+      let myCart = driver.findElement(By.xpath("//h1[.='My Shopping Cart']"))
       await assert.exists(myCart)
     })
+  })
 
-    it('should validate the cart overview', async () => {
+  describe('Cart Validation', async () => {
+    let item_row_quantity
+    let item_row_unit_price
+    let item_row_discount_amount
+    let item_row_discount_total
+    let item_row_regular_total
+
+    let cart_summary_regular_total
+    let cart_summary_discount_total
+    let cart_summary_subtotal
+    let cart_summary_total_items
+
+    it('should have the same quantity of items in the cart as in the cart summary', async () => {
       await driver.wait(until.elementLocated(By.css("#cartview", 10000)))
-      // Get Cart Items per row:
-      var item_row = await driver.findElement(By.xpath("//tr[@class='itemRow cartContents ']"))
-      var item_row_unit_price = await driver.findElement(By.css("td.clr-fix")).getAttribute('innerText')
-      item_row_unit_price = item_row_unit_price.replace().replace('$', '')
-      var item_row_quantity = await driver.findElement(By.xpath("//input[@data-testid='quantityInput']")).getAttribute('value')
-      var item_row_total = await driver.findElement(By.css(".receiptLineTotal")).getText()
-      item_row_total = item_row_total.replace('$', '')
-
-      console.log('unit price', item_row_unit_price)
+      item_row_quantity = await driver.findElement(By.css("[data-testid='quantityInput']")).getAttribute('value')
       console.log('quantity', item_row_quantity)
-      console.log('row total', item_row_total)
+      cart_summary_total_items = await driver.findElement(By.xpath("//td[text()='Total Items:']/following-sibling::td")).getText()
+      console.log('summary items', cart_summary_total_items)
+      assert.equal(item_row_quantity, cart_summary_total_items)
+    })
 
-      // Summary of cart
-      var cart_summary = await driver.findElements(By.xpath('//*[@id="cartview"]//td[contains(@class,"total")]'))
-      var cart_summary_dict = {}
+    it('should be able to validate row discounted total', async () => {
+      item_row_unit_price = await driver.findElement(By.xpath("//span[text()='Unit Price']/following-sibling::span")).getText()
+      item_row_unit_price = parseFloat(item_row_unit_price.replace('$', ''))
+      console.log('unit price', item_row_unit_price)
 
-      for (var i = 2; i < cart_summary.length - 2; i += 2) {
-        var key = await cart_summary[i].getText()
-        // key = key.replace(':', '')
-        console.log(key)
-        var value = await cart_summary[i + 1].getText()
-        value = value.replace('$', '').replace(' USD', '')
-        console.log(value)
-        cart_summary_dict[key] = value
+      try {
+        item_row_discount_amount = await driver.findElement(By.xpath("//span[text()='Discount']/following-sibling::span")).getText()
+        item_row_discount_amount = parseFloat(item_row_discount_amount.replace('-$', ''))
+        console.log('Discount amount', item_row_discount_amount)
+      }
+      catch{
+        item_row_discount_amount = 0.00
+      }
+      try {
+        item_row_discount_total = await driver.findElement(By.css("[class*='discountprice']")).getText()
+        item_row_discount_total = parseFloat(item_row_discount_total.replace('$', ''))
+        console.log('Row Discount Total', item_row_discount_total)
+      }
+      catch{
+        item_row_discount_total = 0.00
+      }
+      assert.equal(item_row_discount_total, item_row_quantity * (item_row_unit_price - item_row_discount_amount))
+    })
+
+    it('should have the discounted total match the summary discounted total', async () => {
+      if (item_row_discount_total !== 0) {
+        cart_summary_discount_total = await driver.findElement(By.xpath("//td[(text()='Y-CASH credit ineligible amount:')]/following-sibling::td")).getText()
+        cart_summary_discount_total = parseFloat(cart_summary_discount_total.replace('$', ''))
+        console.log('Cart Discount Total', cart_summary_discount_total)
+
+      }
+      else {
+        cart_summary_discount_total = 0.00
+      }
+      assert.equal(item_row_discount_total, cart_summary_discount_total)
+    })
+
+    it('should have the same regular row total match the summary regular total', async () => {
+      try {
+        item_row_regular_total = await driver.findElement(By.css("[class*='receiptLineTotal']")).getText()
+        item_row_regular_total = parseFloat(item_row_regular_total.replace('$', ''))
+        console.log('Row Regular Total', item_row_regular_total)
+      }
+      catch{
+        item_row_regular_total = 0.00
       }
 
-      console.log(cart_summary_dict)
+      if (item_row_regular_total !== 0) {
+        cart_summary_regular_total = await driver.findElement(By.xpath("//td[(text()='Y-CASH credit eligible amount:')]/following-sibling::td")).getText()
+        cart_summary_regular_total = parseFloat(cart_summary_regular_total.replace('$', ''))
+        console.log('Cart Regular Total', cart_summary_regular_total)
 
-      assert.equal(item_row_quantity, cart_summary_dict['Total Items:'])
-      assert.equal(item_row_quantity * item_row_unit_price, cart_summary_dict['Subtotal:'])
-      assert.equal(parseFloat(item_row_total) + parseFloat(cart_summary_dict['Shipping:']), cart_summary_dict['Total Balance Due:'])
+      }
+      else {
+        cart_summary_regular_total = 0.00
+      }
+      assert.equal(item_row_regular_total, cart_summary_regular_total)
+    })
+
+    it('should confirm that the row discount total and regular total equal cart subtotal', async () => {
+      cart_summary_subtotal = await driver.findElement(By.xpath("//td[(text()='Subtotal:')]/following-sibling::td")).getText()
+      cart_summary_subtotal = parseFloat(cart_summary_subtotal.replace('$', ''))
+      console.log('Cart SubTotal', cart_summary_subtotal)
+      assert.equal(cart_summary_subtotal, item_row_discount_total + item_row_regular_total)
+    })
+
+    it('should confirm estimated total equal subtotal plus shipping total', async () => {
+      let shipping_cost = await driver.findElement(By.css("[data-testid='shippingDisplay']")).getText()
+      shipping_cost = parseFloat(shipping_cost.replace('$', ''))
+      console.log('Shipping cost', shipping_cost)
 
 
+      let summary_total_balance = await driver.findElement(By.id("carttotal")).getText()
+      summary_total_balance = parseFloat(summary_total_balance.split()[0].replace('$', ''))
+      console.log('Summary Total', summary_total_balance)
 
+      assert.equal(summary_total_balance, cart_summary_subtotal + shipping_cost)
     })
   })
 })
